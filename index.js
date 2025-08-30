8ee73169b613ea4af08d677c36a8d2a10a751427
Aditya7848 <adityas7848@gmail.com>
2025-08-30 20:26:28 +0530

    done using simple fs -> read write append rename

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
-const {styleText} = require('node:util')
-const path = require('path');
-const fs = require('fs');
+const { styleText } = require("node:util");
+const path = require("path");
+const fs = require("fs");
 
+console.log(path.join(__dirname, "files", "starter.txt"));
 
-fs.readFile('./files/starter.txt', (err,data) => {
-    if(err) throw err;
-    console.log(styleText('red',data.toString()));
-})
-console.log('ehllo')
-fs.readFile('./files/starter.txt','utf8', (err,data) => {
-    if(err) throw err;
+console.log("ehllo");
+
+fs.writeFile(
+  path.join(__dirname, "files", "reply.txt"),
+  "This is from writeFile function callback...",
+  (err) => {
+    if (err) throw err;
+    console.log("write complete...");
+
+    fs.appendFile(
+      path.join(__dirname, "files", "reply.txt"),
+      "\n\nthis appending is from appendFile...",
+      (err) => {
+        if (err) throw err;
+        console.log("appending complete...");
+
+        fs.rename(
+          path.join(__dirname, "files", "reply.txt"),
+          path.join(__dirname, "files", "newRenamingReply.txt"),
+          (err) => {
+            if (err) throw err;
+            console.log("renaming complete...");
+          }
+        );
+      }
+    );
+  }
+);
+
+fs.readFile(
+  path.join(__dirname, "files", "newRenamingReply.txt"),
+  "utf8",
+  (err, data) => {
+    if (err) throw err;
     console.log(data);
-})
+  }
+);
 
 //exit on uncaught errors
-process.on('uncaughtException', err => {
-    console.error(`There was an uncaught error : ${err}`);
-    process.exit(1);
-})
+process.on("uncaughtException", (err) => {
+  console.error(`There was an uncaught error : ${err}`);
+  process.exit(1);
+});
