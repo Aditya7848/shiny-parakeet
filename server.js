2d150c81fe3fe1be6e3df81442485f54ff8d7899
Aditya7848 <adityas7848@gmail.com>
2025-09-01 17:52:27 +0530

    first commit

--- server.js
+++ server.js
@@ -1,14 +1,44 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+const fsPromises = require("fs").promises;
+
 const logEvents = require("./logEvents");
-
 const EventEmitter = require("events");
+class Emitter extends EventEmitter {}
+//initialize object
+const myEmitter = new Emitter();
 
-class MyEmitter extends EventEmitter {}
+const PORT = process.env.PORT || 3500;
 
-const myEmitter = new MyEmitter();
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
 
-//add a listener for the log event
-myEmitter.on("log", (msg) => logEvents(msg));
+  let p;
 
-setTimeout(() => {
-  myEmitter.emit("log", "log event emitted!");
-}, 2000);
+  // if (req.url === "/" || req.url === "index.html") {
+  //   res.statusCode = 200;
+  //   res.setHeader("Content-Type", "text/html");
+  //   p = path.join(__dirname, "views", "index.html");
+  //   fs.readFile(p, "utf8", (err, data) => {
+  //     res.end(data);
+
+  //   });
+  // }
+  switch(req.url){
+    case '/':{
+      res.statusCode = 200;
+      res.setHeader = ('Content-Type','text/html');
+      let p = path.join(__dirname, 'views', 'index.html')
+      fs.readFile(p, 'utf8', (err, data) => {
+        res.end(data)
+      })
+      break;
+    }
+  }
+  
+});
+
+server.listen(PORT, () => console.log("server listening in on 3500"));
+// myEmitter.on("log", (msg) => logEvents(msg));
+//   myEmitter.emit("log", "log event emitted!");
