725d232da3b4f759382bcf24125f91f6c27ee20d
Aditya7848 <adityas7848@gmail.com>
2025-09-01 14:54:38 +0530

    npm cli, dev dependencies, scripts, script versions

--- index.js
+++ index.js
@@ -1,40 +1,7 @@
-const { styleText } = require("node:util");
-const path = require("path");
-const fsPromise = require("fs").promises;
+const {format} = require('date-fns')
+const {v4 : uuid} = require('uuid')
 
-const fileOps = async () => {
-  try {
-    const data = await fsPromise.readFile(
-      path.join(__dirname, "files", "starter.txt"),
-      "utf8"
-    );
-    console.log(data);
+console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss'))
 
-    await fsPromise.unlink(path.join(__dirname, "files", "starter.txt"));
-    await fsPromise.writeFile(path.join(__dirname, "files", "promiseWrite.txt"), data);
-    await fsPromise.appendFile(
-      path.join(__dirname, "files", "promiseWrite.txt"),
-      "fist append."
-    );
-    await fsPromise.rename(
-      path.join(__dirname, "files", "promiseWrite.txt"),
-      path.join(__dirname, "files", "promiseComplete.txt")
-    );
-    const newData = await fsPromise.readFile(
-      path.join(__dirname, "files", "promiseComplete.txt"),
-      "utf8"
-    );
-    console.log("newData = ", newData);
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-fileOps();
-
-
-//exit on uncaught errors
-process.on("uncaughtException", (err) => {
-  console.error(`There was an uncaught error : ${err}`);
-  process.exit(1);
-});
+console.log(uuid())
+console.log()
