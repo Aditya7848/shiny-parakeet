55f0bd8c1d87f02dd60da91bf78013d58fee0a62
Aditya7848 <adityas7848@gmail.com>
2025-09-20 21:01:58 +0530

    done serving static files like css and jpeg to the client

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3500;
 
-app.get('/', (req, res) => {
+//middlewares.......
+//built-in middleware to handle urlencoded data in other words, form data:'content-type:application/x-www-form-urlencoded'
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "/public")));
+
+//Routes.............
+app.get("/", (req, res) => {
   // res.sendFile('./views/index.html', {root:__dirname});
-  res.sendFile(path.join(__dirname, 'views','index.html'));
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
 app.get(/^\/new-page(\.html)?$/, (req, res) => {
@@ -16,35 +23,37 @@ app.get(/^\/old-page(\.html)?$/, (req, res) => {
 });
 
 // Route handlers
-app.get(/^\/hello(\.html)?$/, (req, res, next) => {
-    console.log('attempted to load hello.html');
-    next()
-}, (req, res) => {
-    res.send('Hello World!');
-});
-
+app.get(
+  /^\/hello(\.html)?$/,
+  (req, res, next) => {
+    console.log("attempted to load hello.html");
+    next();
+  },
+  (req, res) => {
+    res.send("Hello World!");
+  }
+);
 
 // chaining route handlers
 const one = (req, res, next) => {
-    console.log('one');
-    next();
-}
+  console.log("one");
+  next();
+};
 
 const two = (req, res, next) => {
-    console.log('two');
-    next();
-}
+  console.log("two");
+  next();
+};
 
 const three = (req, res) => {
-    console.log('three');
-    res.send('Finished!');
-}
+  console.log("three");
+  res.send("Finished!");
+};
 
 app.get(/^\/chain(.html)?$/, [one, two, three]);
 
 app.get(/\/*$/, (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-})
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
 
 app.listen(PORT, () => console.log("server listening in on 3500"));
-
